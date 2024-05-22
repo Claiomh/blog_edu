c7f00f43edf9f2a14e83fa521d54f6e838c7c442
Claiomh <riftgofen@gmail.com>
2024-05-22 16:05:22 +0500

    step1 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +18,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+        Category::factory(10)->create();
+      $posts =  Post::factory(10)->create();
+       $tags = Tag::factory(10)->create();
+
+       foreach ($posts as $post) {
+           $tagIDs = $tags->random(5)->pluck('id');
+           $post->tags()->attach($tagIDs);
+       }
     }
 }
