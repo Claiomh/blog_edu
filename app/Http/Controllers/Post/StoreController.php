f7efe56ebb15a65ea3bf1bfadeba1e97d8d5434d
Claiomh <riftgofen@gmail.com>
2024-05-22 11:53:07 +0500

    step 1. 26

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -3,22 +3,16 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
 
-    public function __invoke(Request $request)
+    public function __invoke(StoreRequest $request)
     {
-        $data = $request->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' => 'string',
-            'slug' => 'string',
-            'category_id' => 'integer',
-            'tags' => '',
-        ]);
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
 
--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -3,22 +3,16 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\UpdateRequest;
 use App\Models\Post;
 use App\Models\Tag;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Post $post)
+    public function __invoke(UpdateRequest $request, Post $post)
     {
         $tags = Tag::all();
-        $data = request()->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' => 'string',
-            'slug' => 'string',
-            'category_id' => 'integer',
-            'tags' => '',
-        ]);
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
         $post->update($data);
